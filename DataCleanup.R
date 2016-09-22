b1d2e052ce25207f550eeb281e332bcc8789eae6
Jonathan Berthet <jberthet001@nam.ad.pwcinternal.com>
2016-09-21 23:01:26 -0500

    added linear model

--- DataCleanup.R
+++ DataCleanup.R
@@ -1,45 +1,29 @@
 #Read Files
 setwd("C:/Users/jberthet001/Desktop/AiA/Airbnb/AirbnbApp")
 cal <- read.csv('calendar.csv')
-list <- read.csv('listings.csv')
+listing <- read.csv('listings.csv')
 review <- read.csv('reviews.csv')
+#Save & Read as Rdata file
+saveRDS(list, file = "listingRdata.rds")
+listing <- readRDS("listingRdata.rds")
 
 #Remove dollar signs and turn into integer
 dollarInt <- function(x) {
   as.integer(gsub('\\$', '', x))
 }
 
-columnsDollarInt <- list[,c("price", "weekly_price", "monthly_price", "cleaning_fee", "security_deposit", "extra_people")]
-
-newdf = []
-for (col in columnsDollarInt) {
-  # newdf$col <- dollarInt(col)
-  newdf$col <- as.data.frame(dollarInt(col))
+PercentageInt <- function(x) {
+  as.integer(sub("%", "", x))
 }
-head(newdf)
 
+columnsDollarNum <- listing[,c("price", "weekly_price", "monthly_price", "cleaning_fee", "security_deposit", "extra_people")]   #Columns to convert
+colsPercentageNum <- listing[,c("host_response_rate", "host_acceptance_rate")]
+cleandf1 <- as.data.frame(sapply(columnsDollarInt, dollarInt))
+cleandf2 <- as.data.frame(sapply(colsPercentageNum, PercentageInt))
 
-list$price <- as.integer(gsub('\\$', '', list$price))
-list$weekly_price <- as.integer(gsub('\\$', '', list$weekly_price))
-list$monthly_price <- as.integer(gsub('\\$', '', list$monthly_price))
-list$cleaning_fee <- as.integer(gsub('\\$', '', list$cleaning_fee))
-list$security_deposit <- as.integer(gsub('\\$', '', list$security_deposit))
-list$extra_people <- as.integer(gsub('\\$', '', list$extra_people))
-columns = list[,c("price", "weekly_price")]
-dollarInt(columns)
-```
-
-
-```{r}
-#Linear Model
-mod1 = glm(price ~ review_scores_rating + square_feet, data = list, family = gaussian)
-```
-
-str(list$price)
-str(list$review_scores_rating)
-list_mod1 <- list[,c("price", "review_scores_rating", "square_feet")]
-dim(complete.cases(list_mod1))
-
-Testing
+finaldf <- cbind(cleandf1, cleandf2)
+#Linear Model. Family = poisson because price has poisson shape
+hist(finaldf$price, 1000)
+mod1 = glm(price ~ cleaning_fee + weekly_price + monthly_price + security_deposit + extra_people + host_acceptance_rate + host_response_rate, data = finaldf, family = poisson)
 
 
