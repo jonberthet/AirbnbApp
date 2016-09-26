e7acc78c70eb3c4c10f4a2b051108c890c2e9e09
Jonathan Berthet <jberthet001@nam.ad.pwcinternal.com>
2016-09-25 21:56:56 -0500

    Finished gwss() and spplot() of DC Airbnb Data

--- GeoSpatialWeightedModel_ExampleCode.R
+++ GeoSpatialWeightedModel_ExampleCode.R
@@ -1,6 +1,8 @@
 
 #Geo Spatial Weighted Models paper: http://www.alex-singleton.com/R-Tutorial-Materials/7-converting-coordinates.pdf
 
+
+#Get distance matrix for airbnb price data
 library("GWmodel")
 library("rgdal")
 library("RColorBrewer")
@@ -8,19 +10,36 @@ data("EWHP")
 
 head(ewhp)
 
+LatLon2EastNorthing <- function(latitude, longitude){
+  df <- as.data.frame(latitude, longitude)   #Create df of coordinates
+  df$ID <- 1:nrow(df)                        #create unique ID per coordinate
+  coords <- cbind(Latitude =  as.numeric(as.character(df$latitude)),
+                  Longitude = as.numeric(as.character(df$longitude)))
+  #Create Spatial DF
+  df2 <- SpatialPointsDataFrame(coords,
+                                data = data.frame(listing[,c("price")]),
+                                proj4string = CRS("+init=epsg:4326")) #interpreting latlong
+}
+
+
+
+
+#########################WORKING CODE BELOW
 listLatLon <- listing[,c("latitude", "longitude")]
 listLatLon$ID <- 1:nrow(listLatLon)
+# data <- listing[,c("price", "weekly_price", "availability_30")]
 #Create coordinate variables
-coords <- cbind(Latitude =  as.numeric(as.character(listLatLon$latitude)),
-                Longitude = as.numeric(as.character(listLatLon$longitude)))
+data <- cbind(Latitude =  as.numeric(as.character(listLatLon$latitude)),
+              Longitude = as.numeric(as.character(listLatLon$longitude)),
+              listing[,c("price", "weekly_price", "availability_30")])   #Variables to Use
+data_complete <- data[complete.cases(data),]
+
 #Create Spatial DF
-df <- SpatialPointsDataFrame(coords,
-                             data = data.frame(listing$price),
+df <- SpatialPointsDataFrame(data_complete[,c(1,2)],
+                             data = data.frame(data_complete[,c(3:ncol(data_complete))]),
                              proj4string = CRS("+init=epsg:4326")) #interpreting latlong
 
-# plot(df, col = "red")
-
-# Convert from Longitude & Latitude to Eastings and Northings to Latitude and Longitude
+#Convert from Longitude & Latitude to Eastings and Northings to Latitude and Longitude
 GP_SP_LL <- spTransform(df, CRS("+init=epsg:27700"))
 # we also need to rename the columns
 colnames(GP_SP_LL@coords)[colnames(GP_SP_LL@coords) == "Longitude"] <- "Easting"
@@ -30,23 +49,21 @@ east_northing <- as.data.frame(GP_SP_LL@coords)
 #Variables for holding coordinate system types
 ukgrid = "+init=epsg:27700"
 latlong = "+init=epsg:4326"
-?SpatialPointsDataFrame
 
 
-# data("DubVoter")
-# summary(Dub.voter)
-# class(Dub.voter)
 
+#####EXAMPLE
+data("DubVoter")
+summary(Dub.voter)
+class(Dub.voter)
+
+# gw.ss.bx <- gwss(Dub.voter, vars = c("GenEl2004", "LARent", "Unempl"),kernel = "boxcar", adaptive = TRUE, bw = 48, quantile = TRUE)
 gwssbx <- gwss(df,
-               vars = "listing.price",
-               kernel = "boxcar",
+               vars = c("price", "weekly_price"),   #"availability_30"
+               kernel = "boxcar",    #bocar relates to un-weighted moving window. bisquare relates to weighted moving window. Also try tricube, gaussian, exponential
                adaptive = TRUE,
-               bw = 48,    #bandwidth
-               quantile = TRUE)
-
-
-
-
+               bw = 0.15 * nrow(df),    #roughly 15% of df data
+               quantile = TRUE)    #median, IQR, and quantile imbalance calculated
 
 
 map.na = list("SpatialPolygonsRescale",
@@ -54,21 +71,21 @@ map.na = list("SpatialPolygonsRescale",
               offset = c(400000, 300000),
               scale = 4000,
               col = 1)
+
 map.scale1 <- list("SpatialPolygonsRescale",
                    layout.north.arrow(),
                    offset = c(400000, 300000),
                    scale = 4000,
                    col = 1,
                    fill = c("transparent", "blue"))
+
 map.layout <- list(map.na, map.scale1)
-
-
 
 mypalette1 <- brewer.pal(4, "Reds")
 
 X11(width = 10, height = 12)
 spplot(gwssbx$SDF,
-       "listing.price_LSD",
+       "price_LSD",
        key.space = "right",
        col.regions = mypalette1,
        cuts = 2,
